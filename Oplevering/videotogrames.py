f6f34a65f612e233d96b0ffa0495aa1d6be54872
Ruben <ruben.oosterwijk@hva.nl>
2021-06-29 14:19:31 +0200

    Oplevering map

--- Oplevering/videotogrames.py
+++ Oplevering/videotogrames.py
@@ -2,7 +2,8 @@ import cv2
 import time
 import os
 
-def video_to_frames(input_loc, output_loc):
+
+def video_to_frames(input_loc, output_loc, gray):
     """Function to extract frames from input video file
     and save them as separate frames in an output directory.
     Args:
@@ -21,9 +22,9 @@ def video_to_frames(input_loc, output_loc):
     cap = cv2.VideoCapture(input_loc)
     # Find the number of frames
     video_length = int(cap.get(cv2.CAP_PROP_FRAME_COUNT)) - 1
-    print ("Number of frames: ", video_length)
+    print("Number of frames: ", video_length)
     count = 0
-    print ("Converting video..\n")
+    print("Converting video..\n")
     # Start converting the video
     while cap.isOpened():
         # Extract the frame
@@ -31,23 +32,27 @@ def video_to_frames(input_loc, output_loc):
         if not ret:
             continue
 
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        if gray == 1:
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        else:
+            break
+
         # Write the results back to output location.
-        cv2.imwrite(output_loc + "/%#05d.jpg" % (count+1), gray)
+        cv2.imwrite(output_loc + "/%#05d.jpg" % (count + 1), frame)
         count = count + 1
         # If there are no more frames left
-        if (count > (video_length-1)):
+        if count > (video_length - 1):
             # Log the time again
             time_end = time.time()
             # Release the feed
             cap.release()
             # Print stats
-            print ("Done extracting frames.\n%d frames extracted" % count)
-            print ("It took %d seconds for conversion." % (time_end-time_start))
+            print("Done extracting frames.\n%d frames extracted" % count)
+            print("It took %d seconds for conversion." % (time_end - time_start))
             break
 
-if __name__=="__main__":
-
-    input_loc = 'Play.mp4'
-    output_loc = 'Utrecht/'
-    video_to_frames(input_loc, output_loc)
+if __name__ == "__main__":
+    input_loc = 'Hiambacht.mp4'
+    output_loc = 'Hiambacht_Color/'
+    gray = 1
+    video_to_frames(input_loc, output_loc, gray)
